586d1b68cbca79a1c490286fd9a4705135bd98db
mohanrex <mohan@kuruntham.com>
2015-11-22 16:15:10 +0530

    Settings management implemented
    Settings management have been implemented by using sqlite database.
    Peewee has been used for simplicity and lightweight instead of
    sqlalchemy.

--- Controllers/EKWindow.py
+++ Controllers/EKWindow.py
@@ -1,17 +1,7 @@
-__author__ = 'Raj'
-
-import os
-import shutil
-import time
-import sys
-
-from PyQt5.Qt import QSystemTrayIcon, qApp, QSettings, QGroupBox, QLabel, \
-    QComboBox, QIcon, QHBoxLayout, QCheckBox, QAction, QDialog, QMenu, QVBoxLayout
-from PyQt5.QtCore import qWarning
+from PyQt5.Qt import QSystemTrayIcon, QIcon, QAction, QDialog, QMenu
 from PyQt5.QtGui import QPixmap
-
-from resources import ekalappai_rc
 from view import dialog_ui
+from database import DatabaseManager
 
 
 class EKWindow(QDialog, dialog_ui.Ui_Dialog):
@@ -27,6 +17,9 @@ class EKWindow(QDialog, dialog_ui.Ui_Dialog):
         self.setupUi(self)
         self.construct_tray_icon()
         self.signal_connectors()
+        self.database = DatabaseManager()
+        self.shortcut_key = self.database.get_shortcut_key()
+        self.populate_modifier_cbox()
 
     def construct_tray_icon(self):
         self.tray_icon.setIcon(QIcon(QPixmap(":icon/logo")))
@@ -44,6 +37,8 @@ class EKWindow(QDialog, dialog_ui.Ui_Dialog):
         self.quit_action.triggered.connect(self.quit)
         self.add_new_button.clicked.connect(self.change_dialog_index)
         self.back_button.clicked.connect(self.change_dialog_index)
+        self.modifier_cbox.currentIndexChanged.connect(self.populate_shortcut_key)
+        self.shortcut_key_cbox.currentIndexChanged.connect(self.save_shortcut_key)
 
     def show_about(self):
         pass
@@ -62,3 +57,28 @@ class EKWindow(QDialog, dialog_ui.Ui_Dialog):
             self.stacked_widget.setCurrentIndex(1)
         else:
             self.stacked_widget.setCurrentIndex(0)
+
+    def populate_modifier_cbox(self):
+        self.modifier_cbox.blockSignals(True)
+        modifiers = self.database.get_keys()
+        for modifier in modifiers:
+            self.modifier_cbox.addItem(modifier.name, modifier.id)
+            if modifier.id == self.shortcut_key.parent.id:
+                self.modifier_cbox.setCurrentText(modifier.name)
+        self.populate_shortcut_key()
+        self.modifier_cbox.blockSignals(False)
+
+    def populate_shortcut_key(self):
+        self.shortcut_key_cbox.blockSignals(True)
+        self.shortcut_key_cbox.clear()
+        keys = self.database.get_keys(self.modifier_cbox.currentData())
+        for key in keys:
+            self.shortcut_key_cbox.addItem(key.name, key.id)
+            if key.id == self.shortcut_key.id:
+                self.shortcut_key_cbox.setCurrentText(key.name)
+        self.shortcut_key_cbox.blockSignals(False)
+        self.save_shortcut_key()
+
+    def save_shortcut_key(self):
+        self.database.set_shortcut_key(self.shortcut_key_cbox.currentData())
+        self.shortcut_key = self.database.get_shortcut_key()
---
+++ database/DatabaseManager.py
@@ -0,0 +1,106 @@
+from database.Tables import *
+
+
+class DatabaseManager:
+    def __init__(self):
+        self.db = database
+        self.init_data = [
+                {'name': 'CTRL', 'parent': '0'},
+                {'name': 'ALT', 'parent': '0'},
+                {'name': 'NONE', 'parent': '0'},
+                {'name': '1', 'parent': '1'},
+                {'name': '2', 'parent': '1'},
+                {'name': '3', 'parent': '1'},
+                {'name': '4', 'parent': '1'},
+                {'name': '5', 'parent': '1'},
+                {'name': '6', 'parent': '1'},
+                {'name': '7', 'parent': '1'},
+                {'name': '8', 'parent': '1'},
+                {'name': '9', 'parent': '1'},
+                {'name': '0', 'parent': '1'},
+                {'name': '1', 'parent': '2'},
+                {'name': '2', 'parent': '2'},
+                {'name': '3', 'parent': '2'},
+                {'name': '4', 'parent': '2'},
+                {'name': '5', 'parent': '2'},
+                {'name': '6', 'parent': '2'},
+                {'name': '7', 'parent': '2'},
+                {'name': '8', 'parent': '2'},
+                {'name': '9', 'parent': '2'},
+                {'name': '0', 'parent': '2'},
+                {'name': 'F1', 'parent': '1'},
+                {'name': 'F2', 'parent': '1'},
+                {'name': 'F3', 'parent': '1'},
+                {'name': 'F4', 'parent': '1'},
+                {'name': 'F5', 'parent': '1'},
+                {'name': 'F6', 'parent': '1'},
+                {'name': 'F7', 'parent': '1'},
+                {'name': 'F8', 'parent': '1'},
+                {'name': 'F9', 'parent': '1'},
+                {'name': 'F10', 'parent': '1'},
+                {'name': 'F11', 'parent': '1'},
+                {'name': 'F12', 'parent': '1'},
+                {'name': 'F1', 'parent': '2'},
+                {'name': 'F2', 'parent': '2'},
+                {'name': 'F3', 'parent': '2'},
+                {'name': 'F4', 'parent': '2'},
+                {'name': 'F5', 'parent': '2'},
+                {'name': 'F6', 'parent': '2'},
+                {'name': 'F7', 'parent': '2'},
+                {'name': 'F8', 'parent': '2'},
+                {'name': 'F9', 'parent': '2'},
+                {'name': 'F10', 'parent': '2'},
+                {'name': 'F11', 'parent': '2'},
+                {'name': 'F12', 'parent': '2'},
+                {'name': 'F1', 'parent': '3'},
+                {'name': 'F2', 'parent': '3'},
+                {'name': 'F3', 'parent': '3'},
+                {'name': 'F4', 'parent': '3'},
+                {'name': 'F5', 'parent': '3'},
+                {'name': 'F6', 'parent': '3'},
+                {'name': 'F7', 'parent': '3'},
+                {'name': 'F8', 'parent': '3'},
+                {'name': 'F9', 'parent': '3'},
+                {'name': 'F10', 'parent': '3'},
+                {'name': 'F11', 'parent': '3'},
+                {'name': 'F12', 'parent': '3'},
+            ]
+        try:
+            initialize()
+            self.init_tables()
+        except OperationalError:
+            pass
+        self.db.connect()
+
+    @staticmethod
+    def create_setting(values):
+        record = GeneralSetting(name=values['name'], value1=values['value1'])
+        record.save()
+
+    @staticmethod
+    def get_keys(parent=0):
+        return ShortcutKey.select().where(ShortcutKey.parent == parent)
+
+    @staticmethod
+    def set_shortcut_key(key_id):
+        shortcut_key = GeneralSetting.get(name="shortcut_key")
+        shortcut_key.value1 = str(key_id)
+        shortcut_key.save()
+
+    @staticmethod
+    def get_shortcut_key():
+        shortcut_key, created = GeneralSetting.get_or_create(name="shortcut_key", defaults={'value1': 4})
+        print(shortcut_key.value1)
+        modifier = ShortcutKey.alias()
+        shortcut_key_model = (ShortcutKey
+                              .select(ShortcutKey, modifier)
+                              .join(modifier, on=(ShortcutKey.parent == modifier.id))
+                              .where(ShortcutKey.id == shortcut_key.value1)).first()
+        return shortcut_key_model
+
+    def init_tables(self):
+        with self.db.atomic():
+            ShortcutKey.insert_many(self.init_data).execute()
+
+    def __del__(self):
+        self.db.close()
